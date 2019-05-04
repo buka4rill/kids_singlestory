25750a0c458ade519460e490ce4dcb7099e148e4
Ebuka Abraham <45390219+buka4rill@users.noreply.github.com>
2019-05-04 12:15:57 +0100

    Add files via upload
    BE to be done by @Daud

---
+++ singlestory/singlestory.php
@@ -0,0 +1,148 @@
+<?php include 'partials/header.php'; ?>
+
+<link rel="stylesheet" type="text/css" media="screen" href="singlecss/singlestory.css">
+<link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet">
+
+    
+<body>
+    <div class="page-wrapper">
+        <!-- Main Navigation-->
+            <?php include 'partials/navbar.php'; ?>
+        <!--End Main Navigation -->
+
+
+        <div class="content">
+            <!-- Showcase -->
+            <section class="top-container">
+                    <header class="showcase">
+                        <h1 class="text-white">  </h1>
+                    </header>
+            </section>
+
+            <!-- Begin -->
+            <span class="content1 topic">
+                <h1 >The Legend of The Dragon That Fell From The Sky</h1>
+            </span>
+
+            <br>
+
+            <div class="content1">
+
+                <div class="">
+                    <img class="pull-left dragon" src="https://res.cloudinary.com/solape/image/upload/v1556844781/Screen_Shot_2019-05-03_at_1.52.35_AM.png">              
+                    <h2> What Is Lorem Ipsum </h2>
+                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has
+                    been the industry's standard dummy text ever since the 1500s, "it has survived not only five 
+                    centuries, but also the leap into electronic typesetting, remaining essentially unchanged."
+                    When an unknown printer took a galley of type and scrambled it to make a type specimen book. publishing software like Aldus PageMaker including versions
+                    of Lorem Ipsum publishing software like Aldus PageMaker including versions of
+                    Lorem Ipsum.            
+                    "Very popular during the Renaissance. The first line of..."
+                    Looked up one of the most obscure Latin words,
+                    consectetur, from a Lorem Ipsum passage, and going through the cities of
+                    the world in classical literature,
+                    very popular in the Renaissance.
+                    "Very popular during the Renaissance. The first line of..."
+                    Looked up one of the most obscure Latin words,
+                    consectetur, from a Lorem Ipsum passage, and going through the cities of
+                    the world in classical literature,
+                    very popular in the Renaissance.
+                    "Very popular during the Renaissance. The first line of..."
+                    Looked up one of the most obscure Latin words,
+                    consectetur, from a Lorem Ipsum passage, and going through the cities of
+                    the world in classical literature,
+                    very popular in the Renaissance.
+                    <img class="pull-right dragon2" src="https://res.cloudinary.com/solape/image/upload/v1556481719/dragon.svg">
+                    "Very popular during the Renaissance. The first line of..."
+                    Looked up one of the most obscure Latin words,
+                    consectetur, from a Lorem Ipsum passage, and going through the cities of
+                    the world in classical literature,
+                    very popular in the Renaissance.
+                    "Very popular during the Renaissance. The first line of..."
+                    Looked up one of the most obscure Latin words,
+                    consectetur, from a Lorem Ipsum passage, and going through the cities of
+                    the world in classical literature,
+                    very popular in the Renaissance.
+                   
+                    "Very popular during the Renaissance. The first line of..."
+                    Looked up one of the most obscure Latin words,
+                    consectetur, from a Lorem Ipsum passage, and going through the cities of
+                    the world in classical literature,
+                    very popular in the Renaissance.
+                    "Very popular during the Renaissance. The first line of..."
+                    Looked up one of the most obscure Latin words,
+                    consectetur, from a Lorem Ipsum passage, and going through the cities of
+                    the world in classical literature,
+                    very popular in the Renaissance.
+                
+                    The first line of Lorem Ipsum, "Lorem Ipsum dolor sit
+                    </p>
+                    
+                </div>
+
+                <h1 class="end">THE END</h1>
+
+
+                <div class="tags">
+                    <div>
+                        <button class="" type="submit" id="submit"> dragon </button>
+                        <button type="submit" id="submit"> history </button>
+                        <button type="submit" id="submit"> dragon </button>
+                        <button type="submit" id="submit"> history </button> 
+                        <i class="fa fa-thumbs-down likes"><span> 627 </span></i>
+                        <i class="fa fa-thumbs-up likes"><span> 3255 </span></i>
+                    </div>
+                </div>
+
+
+                <hr>
+
+                <h2 class="h2-stories"> Stories You Might Like </h2>
+                <section id="story">
+                    <img class="pull-left stories" src="https://res.cloudinary.com/solape/image/upload/v1556534093/Myth.svg" class="img">
+                    <h2>The Legend of The Dragon That Fell From The Sky</h2>
+                        <p>The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form accompanied by English versions from the 1914 translation by H. Rackham.
+                        <br>
+                        <span>
+                        <a href="#"> Read More... </a>
+                        </span>
+                        </p>
+            
+                    <img class="pull-left stories" src="https://res.cloudinary.com/solape/image/upload/v1556534093/Myth.svg" class="img">
+                    <h2>The Legend of The Dragon That Fell From The Sky</h2>
+                        <p>The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form accompanied by English versions from the 1914 translation by H. Rackham.
+                        <br>
+                        <span>
+                        <a href="#"> Read More... </a>
+                        </span>
+                        </p>
+            
+            
+                    <img class="pull-left stories" src="https://res.cloudinary.com/solape/image/upload/v1556534093/Myth.svg" class="img">
+                    <h2>The Legend of The Dragon That Fell From The Sky</h2>
+                        <p>The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form accompanied by English versions from the 1914 translation by H. Rackham.
+                        <br>
+                        <span>
+                        <a href="#"> Read More... </a>
+                        </span>
+                        </p>
+                </section>                                
+            </div>
+
+        </div>
+
+
+
+    <!-- Footer goes here -->
+    <?php include 'partials/footer.php'; ?>
+
+    <!--Scroll to top-->
+    <div class="scroll-to-top scroll-to-target" data-target="html"><span class="icon fa fa-angle-double-up"></span></div>
+
+    <!-- Scripts -->
+    <script src="tjs/paroller.js"></script>
+    <script src="tjs/script.js"></script>
+</body>
+</html>
+
+
